dcd8d6878aab594f39ecf521a34f633548e3e5cc
ShreyaDayma-cse <shreyadayma0901@gmail.com>
2022-07-18 18:55:26 +0530

    first

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+var list = document.querySelector('#list');
+var button = document.querySelector('.btn');
+var income = 0.00;
+var expense = 0.00;
+function display() {
+    for (var i = 0; i < localStorage.length; i++) {
+        var transactionList = document.createElement('li');
+        var amount = localStorage.getItem(localStorage.key(i));
+        var input = localStorage.key(i);
+        if (amount > 0) {
+            income = income + parseInt(amount);
+            transactionList.setAttribute('class', 'plus');
+            transactionList.textContent = input + "---------------------------------" + amount;
+        } else {
+            expense -= amount;
+            transactionList.setAttribute('class', 'minus');
+            transactionList.textContent = input + "---------------------------------" + amount;
+        }
+        list.appendChild(transactionList);
+    }
+    document.querySelector('#money-plus').textContent = '$+  ' + income;
+    document.querySelector('#money-minus').textContent = '$-  ' + expense;
+    document.querySelector('#balance').textContent = '$  ' + (income - expense);
+}
 
+button.addEventListener('click', addItems);
+
+
+function addItems() {
+    var input = document.querySelector('#text').value;
+    var amount = document.querySelector('#amount').value;
+    localStorage.setItem(input, amount);
+}
+
+window.onload = () => {
+    display();
+}
